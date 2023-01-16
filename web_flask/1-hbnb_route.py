e05e1740588c15c16bd264e95b0a7652374fceaf
lonjie254 <105755934+lonjie254@users.noreply.github.com>
2023-01-16 12:11:38 +0300

    1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A script that start a Flash web application.
+    It's listening on 0.0.0.0, port 5000
+     Routes:
+        /: Displays 'Hello HBNB!'
+        /hbnb: Displays 'HBNB'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello(strict_slashes=False):
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hello_bnb(strict_slashes=False):
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
