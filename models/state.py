ddb5e9330db1541628e7701fa19d4de9d375c2ab
lonjie254 <105755934+lonjie254@users.noreply.github.com>
2023-01-16 12:22:52 +0300

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+import models
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+
+from models.city import City
+
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """ Gets a list of all cities in state """
+            return [city for city in models.storage.all(City).values() if
+                    self.id == city.state_id]
